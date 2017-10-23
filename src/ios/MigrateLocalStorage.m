e818febb477da79f8687df75d0fa9ab3d1bcd262
Jérémy Li <mail.jeremy.li@gmail.com>
2017-10-23 12:14:43 +1100

    adapated plugin for cordova-plugin-ionic-webview on port 9002

--- src/ios/MigrateLocalStorage.m
+++ src/ios/MigrateLocalStorage.m
@@ -1,5 +1,14 @@
 #import "MigrateLocalStorage.h"
 
+#define TAG @"ジェレミー"
+#define ORIG_LS_FILEPATH @"WebKit/LocalStorage/file__0.localstorage"
+#define ORIG_LS_CACHE @"Caches/file__0.localstorage"
+// #define TARGET_LS_FILEPATH @"WebsiteData/LocalStorage/file__0.localstorage"
+#define TARGET_LS_FILEPATH @"WebsiteData/LocalStorage/http_localhost_9002.localstorage"
+
+// TODO: test IndexedDB
+// TODO: test remigration
+
 @implementation MigrateLocalStorage
 
 - (BOOL) copyFrom:(NSString*)src to:(NSString*)dest
@@ -8,60 +17,96 @@
 
     // Bail out if source file does not exist
     if (![fileManager fileExistsAtPath:src]) {
+        NSLog(@"%@ Source file does not exist", TAG);
         return NO;
     }
 
     // Bail out if dest file exists
     if ([fileManager fileExistsAtPath:dest]) {
+        NSLog(@"%@ Target file exists", TAG);
         return NO;
     }
 
     // create path to dest
     if (![fileManager createDirectoryAtPath:[dest stringByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil]) {
+        NSLog(@"%@ error creating target file", TAG);
         return NO;
     }
 
     // copy src to dest
     return [fileManager copyItemAtPath:src toPath:dest error:nil];
+    // return YES;
 }
 
-- (void) migrateLocalStorage
+/**
+* Gets filepath of localStorage file we want to copy from
+*/
+- (NSString*) resolveOriginalFile
 {
-    // Migrate UIWebView local storage files to WKWebView. Adapted from
-    // https://github.com/Telerik-Verified-Plugins/WKWebView/blob/master/src/ios/MyMainViewController.m
-
     NSString* appLibraryFolder = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString* original;
 
-    if ([[NSFileManager defaultManager] fileExistsAtPath:[appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage/file__0.localstorage"]]) {
-        original = [appLibraryFolder stringByAppendingPathComponent:@"WebKit/LocalStorage"];
-    } else {
-        original = [appLibraryFolder stringByAppendingPathComponent:@"Caches"];
-    }
+    NSString* originalLSFilePath = [appLibraryFolder stringByAppendingPathComponent:ORIG_LS_FILEPATH];
 
-    original = [original stringByAppendingPathComponent:@"file__0.localstorage"];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:originalLSFilePath]) {
+        original = originalLSFilePath;
+    } else {
+        original = [appLibraryFolder stringByAppendingPathComponent:ORIG_LS_CACHE];
+    }
+    return original;
+}
+
+/**
+* Gets filepath of localStorage file we want to copy to
+*/
+- (NSString*) resolveTargetFile
+{
+    NSString* appLibraryFolder = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
 
     NSString* target = [[NSString alloc] initWithString: [appLibraryFolder stringByAppendingPathComponent:@"WebKit"]];
+    #if TARGET_IPHONE_SIMULATOR
+        NSLog(@"%@ 🌕 I am a simulator", TAG);
+        // the simulutor squeezes the bundle id into the path
+        NSString* bundleIdentifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+        target = [target stringByAppendingPathComponent:bundleIdentifier];
+    #endif
 
-#if TARGET_IPHONE_SIMULATOR
-    // the simulutor squeezes the bundle id into the path
-    NSString* bundleIdentifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-    target = [target stringByAppendingPathComponent:bundleIdentifier];
-#endif
+    return [target stringByAppendingPathComponent:TARGET_LS_FILEPATH];
+}
 
-    target = [target stringByAppendingPathComponent:@"WebsiteData/LocalStorage/file__0.localstorage"];
+/**
+* Checks if localStorage file should be migrated. If so, migrate.
+* NOTE: Will only migrate data if there is no localStorage data for WKWebView. This only happens when WKWebView is set up for the first time.
+*/
+- (void) migrateLocalStorage
+{
+    // Migrate UIWebView local storage files to WKWebView. Adapted from
+    // https://github.com/MaKleSoft/cordova-plugin-migrate-localstorage and
+    // https://github.com/Telerik-Verified-Plugins/WKWebView/blob/master/src/ios/MyMainViewController.m
+
+    NSString* original = [self resolveOriginalFile];
+    NSLog(@"%@ 📦 original %@", TAG, original);
+
+    NSString* target = [self resolveTargetFile];
+    NSLog(@"%@ 🏹 target %@", TAG, target);
 
     // Only copy data if no existing localstorage data exists yet for wkwebview
     if (![[NSFileManager defaultManager] fileExistsAtPath:target]) {
-        NSLog(@"No existing localstorage data found for WKWebView. Migrating data from UIWebView");
-        [self copyFrom:original to:target];
-        [self copyFrom:[original stringByAppendingString:@"-shm"] to:[target stringByAppendingString:@"-shm"]];
-        [self copyFrom:[original stringByAppendingString:@"-wal"] to:[target stringByAppendingString:@"-wal"]];
+        NSLog(@"%@ 🕐 No existing localstorage data found for WKWebView. Migrating data from UIWebView", TAG);
+        BOOL success = [self copyFrom:original to:target];
+        BOOL success2 = [self copyFrom:[original stringByAppendingString:@"-shm"] to:[target stringByAppendingString:@"-shm"]];
+        BOOL success3 = [self copyFrom:[original stringByAppendingString:@"-wal"] to:[target stringByAppendingString:@"-wal"]];
+        NSLog(@"%@ copy status %d %d %d", TAG, success, success2, success3);
+    }
+    else {
+        // NSLog(@"%@ ⚪️ found data. not migrating", TAG);
+        NSLog(@"%@ 🔴 found data. STILL migrating", TAG);
     }
 }
 
 - (void)pluginInitialize
 {
+    NSLog(@"%@ ✅ plugin initialised", TAG);
     [self migrateLocalStorage];
 }
 
